c34c72adc160cd33cc60d5b6d597aaee0bc509ff
Kartikay Dubey <kartikaydubey5074@gmail.com>
2025-09-07 17:35:29 +0530

    Assignment Uploaded!

--- Pattern Problems/PerfectTriangle.py
+++ Pattern Problems/PerfectTriangle.py
@@ -1,6 +1,6 @@
 N=int(input("Enter rows:"))
 for i in range(N):
-    for j in range(N-I-1):
+    for j in range(N-i-1):
         print(" ",end="")
     for j in range(i+1):
         print("*",end=" ")
